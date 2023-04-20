75b097095f4e3b638e91987dd97ca7701654700e
Brijesh Chaudhary <75580977+brijeshc1307@users.noreply.github.com>
2023-04-20 17:38:37 +0530

    Update Reverse of string.cpp

--- DSA Basic to Adavnced/Reverse of string.cpp
+++ DSA Basic to Adavnced/Reverse of string.cpp
@@ -1,13 +1,19 @@
+## Program Code
+
+```c++
 #include <iostream>
 using namespace std;
 
+// Function to reverse the string
 void reversestring(char name[], int n){
-  int s=0;
-  int e=n-1;
-  while(s<e){
-    swap(name[s++],name[e--]);
-  }
+    int s=0;
+    int e=n-1;
+    while(s<e){
+        swap(name[s++],name[e--]);
+    }
 }
+
+// Function to get the length of the string
 int getlength(char name[]){
     int count=0;
     for(int i=0; name[i] != '\0'; i++){
@@ -16,14 +22,32 @@ int getlength(char name[]){
     return count;
 }
 
+// Main function
 int main() {
     char name[20];
-    cout<<"Enter vour name "<<endl;
+    cout<<"Enter your name: "<<endl;
     cin>>name;
-    cout<< "Your names is "<<name<<endl;
-  int len=getlength(name);
-    cout<<"Lenath of string is "<<len<< endl;
-  reversestring(name,len);
+    cout<< "Your name is "<<name<<endl;
+    int len=getlength(name);
+    cout<<"Length of string is "<<len<< endl;
+    reversestring(name,len);
     cout<<"Reverse of string is "<<name<<endl;
-	return 0;
+    return 0;
 }
+```
+
+## Sample Output
+
+```
+Enter your name: John
+Your name is John
+Length of string is 4
+Reverse of string is nhoJ
+```
+
+## Notes
+
+- The program assumes that the maximum length of the input string is 20 characters.
+- The `reversestring` function modifies the original string in place.
+- The `getlength` function counts the number of characters in the string until it encounters the null character `\0`, which marks the end of the string.
+- The program uses the `swap` function from the `<algorithm>` header to swap the characters in the string.
