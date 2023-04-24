58d89a77c2e03f61f6eb894f1c0d8397d446660d
Brijesh Chaudhary <75580977+brijeshc1307@users.noreply.github.com>
2023-04-24 12:51:13 +0530

    Create Permutation in String.cpp

---
+++ DSA Basic to Adavnced/Permutation in String.cpp
@@ -0,0 +1,63 @@
+/* Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+In other words, return true if one of s1's permutations is the substring of s2.
+
+Example 1:
+
+Input: s1 = "ab", s2 = "eidbaooo"
+Output: true
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+
+Input: s1 = "ab", s2 = "eidboaoo"
+Output: false
+*/
+
+class Solution {
+public:
+    bool checkequal(int a[26],int b[26]){
+        for(int i=0; i<26; i++){
+            if(a[i]!=b[i])
+                return 0;
+        }
+        return 1;
+    }
+
+
+    bool checkInclusion(string s1, string s2) {
+        //character count array
+        int count1[26]={0};
+        for(int i=0; i<s1.length(); i++){
+            int index=s1[i]-'a';
+            count1[index]++;
+        }
+
+    //traverse s2 string in window of size s1 length and compare
+    int i=0;
+    int windowsize=s1.length();
+    int count2[26]={0};
+    
+      // running for first window
+    while(i< windowsize && i<s2.length()){
+        int index=s2[i]-'a';
+        count2[index]++;
+        i++;
+    }
+    if(checkequal(count1,count2))
+        return 1;
+  // aage window process karo
+    while(i<s2.length()){
+        char newchar=s2[i];
+        int index=newchar-'a';
+        count2[index]++;
+        char oldchar=s2[i-windowsize];
+        index=oldchar-'a';
+        count2[index]--;
+        i++;
+        
+        if(checkequal(count1,count2))
+            return 1;
+        }
+    return 0;
+    }
+};
