da0b5178ea61c19c342c964d7d0481934d7a361f
Brijesh Chaudhary <75580977+brijeshc1307@users.noreply.github.com>
2023-05-04 13:21:29 +0530

    Update pattern1.cpp

--- DSA Basic to Adavnced/pattern1.cpp
+++ DSA Basic to Adavnced/pattern1.cpp
@@ -24,3 +24,23 @@ int main() {
 }
 
 // TC : O(n^2)
+
+
+
+// Using While
+
+int main() {
+  int n;
+  cin>>n;
+  int i=1;
+  while(i<=n){
+    int j=1;
+    while(j<=n){
+      cout<<j<<" ";
+      j++;
+    }
+    cout<<endl;
+    i++;
+  }
+  return 0;
+}
