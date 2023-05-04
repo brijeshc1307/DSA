6bd401495c7f9d9b9ea5400c9024f1c3db074508
Brijesh Chaudhary <75580977+brijeshc1307@users.noreply.github.com>
2023-05-04 14:42:57 +0530

    Create pattern3.cpp

---
+++ DSA Basic to Adavnced/pattern3.cpp
@@ -0,0 +1,47 @@
+/*
+3
+1 
+2 2 
+3 3 3 
+*/
+
+/*
+3
+* 
+* * 
+* * * 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin>>n;
+  for(int i=1; i<=n;i++){
+    for(int j=1;j<=i;j++){
+      cout<<i<<" ";
+    }
+    cout<<endl;
+  }
+  return 0;
+}
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin>>n;
+  for(int i=1; i<=n;i++){
+    for(int j=1;j<=i;j++){
+      cout<<"*"<<" ";
+    }
+    cout<<endl;
+  }
+  return 0;
+}
+
+
+// TC = O(n^2)
