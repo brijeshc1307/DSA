b3572372245d50d277557fb8212394db00181b4f
Brijesh Chaudhary <75580977+brijeshc1307@users.noreply.github.com>
2023-05-23 18:08:21 +0530

    Update SingleLinklistInsertion.cpp

--- DSA Basic to Adavnced/SingleLinklistInsertion.cpp
+++ DSA Basic to Adavnced/SingleLinklistInsertion.cpp
@@ -26,16 +26,21 @@ void InsertAtTail(Node* &tail, int d){
   tail=tail->next;   // tail=temp;// bhi kar sakte h
 }
 
-void InsertAtPosition(Node* &head, int position, int d){
+void InsertAtPosition(Node* &head, Node* &tail, int position, int d){
   if(position==1){
     InsertAtHead(head,d);
     return;
   }
+  
   Node* temp= head;
   int cnt=1;
   while(cnt<position-1){
     temp=temp->next;
     cnt++;
+  }
+  if(temp->next==NULL){
+    InsertAtTail(tail,d);
+    return;
   }
   // Create new node d
   Node* nodetoinsert = new Node(d);
@@ -71,7 +76,10 @@ int main() {
   InsertAtTail(tail, 30);
   InsertAtTail(tail, 40);
   print(head);
-  InsertAtPosition(head, 4, 100);
+  InsertAtPosition(head,tail, 8, 100);
   print(head);
+
+  cout<<head->data<<endl;
+  cout<<tail->data<<endl;
     return 0;
 }
