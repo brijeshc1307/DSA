e5dcb78643bdb645d29daab897deee7c30d3ad91
Brijesh Chaudhary <75580977+brijeshc1307@users.noreply.github.com>
2023-05-29 15:40:05 +0530

    Create construtor.cpp

---
+++ DSA Basic to Adavnced/construtor.cpp
@@ -0,0 +1,114 @@
+// Constructor
+
+#include<iostream>
+using namespace std;
+
+class Shape
+{
+    public:
+      int height;
+  
+    public:
+      int width;
+
+    Shape(){
+        cout<<"Constructor Called"<<endl;
+      }
+      void show(){
+        cout<<width<<endl;
+      }
+      
+};
+
+
+int main()
+{
+  // object creation statically
+    Shape shape;
+
+    // object creation Dynamicaly
+    Shape  *d = new Shape;
+
+    return 0;
+}
+
+// Parmetrized Constructor
+
+// C++ program to calculate the area of a wall
+
+#include <iostream>
+using namespace std;
+
+// declare a class
+class Wall {
+  private:
+    double length;
+    double height;
+
+  public:
+    // parameterized constructor to initialize variables
+    Wall(double len, double hgt) {
+      length = len;
+      height = hgt;
+    }
+
+    double calculateArea() {
+      return length * height;
+    }
+};
+
+int main() {
+  // create object and initialize data members
+  Wall wall1(10.5, 8.6);
+  Wall wall2(8.5, 6.3);
+
+  cout << "Area of Wall 1: " << wall1.calculateArea() << endl;
+  cout << "Area of Wall 2: " << wall2.calculateArea();
+
+  return 0;
+}
+
+//  Copy Cunstrctor
+
+#include <iostream>
+using namespace std;
+
+// declare a class
+class Wall {
+  private:
+    double length;
+    double height;
+
+  public:
+
+    // initialize variables with parameterized constructor
+    Wall(double len, double hgt) {
+      length = len;
+      height = hgt;
+    }
+
+    // copy constructor with a Wall object as parameter
+    // copies data of the obj parameter
+    Wall(Wall &obj) {
+      length = obj.length;
+      height = obj.height;
+    }
+
+    double calculateArea() {
+      return length * height;
+    }
+};
+
+int main() {
+  // create an object of Wall class
+  Wall wall1(10.5, 8.6);
+
+  // copy contents of wall1 to wall2
+  Wall wall2 = wall1;
+
+  // print areas of wall1 and wall2
+  cout << "Area of Wall 1: " << wall1.calculateArea() << endl;
+  cout << "Area of Wall 2: " << wall2.calculateArea();
+
+  return 0;
+}
