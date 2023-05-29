6e53e04d85912604bcd4a8463480121e968fb9a1
Brijesh Chaudhary <75580977+brijeshc1307@users.noreply.github.com>
2023-05-29 13:07:05 +0530

    Create ClassObject.cpp

---
+++ DSA Basic to Adavnced/ClassObject.cpp
@@ -0,0 +1,17 @@
+/// Note empty class ki size 1 byte hota h
+// Class:
+// Object:
+
+#include <iostream>
+using namespace std;
+
+class Person{
+// Properties
+  
+};
+int main() {
+  // creation of object
+  Person p;
+  cout<<"Size of Class Person : "<<sizeof(p)<<endl;
+  return 0;
+}
