a361ff2d7d4c24481863cbeeced22519e464446f
Brijesh Chaudhary <75580977+brijeshc1307@users.noreply.github.com>
2023-05-29 17:16:25 +0530

    Create encapsulation.cpp

---
+++ DSA Basic to Adavnced/encapsulation.cpp
@@ -0,0 +1,13 @@
+class MyClass {
+private:
+    int privateData;
+
+public:
+    void setPrivateData(int value) {
+        privateData = value;
+    }
+
+    int getPrivateData() {
+        return privateData;
+    }
+};
